b4460b1b36210d7adc4c193bce56f233cbdcbf97
Andreas Monitzer <a.monitzer@dungeonfog.com>
2020-10-18 23:42:47 +0200

    Use snake_case for everything in the websocket communication.

--- tttod_data/src/game_state.rs
+++ tttod_data/src/game_state.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Copy)]
+#[serde(rename_all = "snake_case")]
 pub enum GameState {
     PlayerSelection,
     DefineEvil,
