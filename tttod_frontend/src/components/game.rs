0e0428dc9e43ec96363426c4bb14b39a860c1ce2
Andreas Monitzer <a.monitzer@dungeonfog.com>
2020-10-18 23:43:13 +0200

    Open websocket connection when the game is started on the client.

--- tttod_frontend/src/components/game.rs
+++ tttod_frontend/src/components/game.rs
@@ -2,6 +2,8 @@ mod lobby;
 pub use lobby::Lobby;
 
 use uuid::Uuid;
+use wasm_bindgen_futures::spawn_local;
+use ws_stream_wasm::WsMeta;
 use yew::prelude::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -17,6 +19,7 @@ pub struct Game {
     props: Props,
     state: GameState,
     player_id: Uuid,
+    websocket: Option<WsMeta>,
 }
 
 #[derive(Debug, Clone, Properties)]
@@ -26,6 +29,7 @@ pub struct Props {
 
 pub enum Msg {
     SetPlayerName(String),
+    SetWebsocket(WsMeta),
 }
 
 fn local_storage() -> web_sys::Storage {
@@ -50,11 +54,32 @@ impl Component for Game {
                 .unwrap();
             player_id
         };
+        let inner_link = link.clone();
+        let game_name = props.game_name.clone();
+        spawn_local(async move {
+            let base = {
+                let host = "localhost:8081"; //web_sys::window().unwrap().location().host().unwrap();
+                if web_sys::window().unwrap().location().protocol().unwrap() == "https:" {
+                    format!("wss://{}", host)
+                } else {
+                    format!("ws://{}", host)
+                }
+            };
+            if let Ok((meta, _)) = WsMeta::connect(
+                &format!("{}/api/{}/{}/ws", base, game_name, player_id),
+                None,
+            )
+            .await
+            {
+                inner_link.send_message(Msg::SetWebsocket(meta));
+            }
+        });
         Self {
             link,
             props,
             state: GameState::PlayerSelection,
             player_id,
+            websocket: None,
         }
     }
 
@@ -63,6 +88,10 @@ impl Component for Game {
             Msg::SetPlayerName(name) => {
                 // TODO
                 true
+            }
+            Msg::SetWebsocket(meta) => {
+                self.websocket = Some(meta);
+                false
             }
         }
     }
