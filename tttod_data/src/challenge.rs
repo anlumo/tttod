a21129c841125812516def851d1c6dddb9b6505f
Andreas Monitzer <andreas@monitzer.com>
2020-10-21 03:54:55 +0200

    One more file.

---
+++ tttod_data/src/challenge.rs
@@ -0,0 +1,11 @@
+use crate::Attribute;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Challenge {
+    pub player_id: Uuid,
+    pub attribute: Attribute,
+    pub speciality_applies: bool,
+    pub reputation_applies: bool,
+}
